37b8997eb69a4c264b073f292896f917115c66af
Oleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-04-17 00:42:54 +0900

    Add files via upload

---
+++ lab_7.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    // Открываем файл с исходным кодом на языке Си
+    ifstream inputFile("input.c");
+
+    if (!inputFile.is_open()) {
+        cout << "Не удалось открыть файл" << endl;
+        return 1;
+    }
+
+    // Создаем стек для хранения открывающихся фигурных скобок
+    stack<int> stack;
+
+    // Считываем файл посимвольно и проверяем скобки
+    char ch;
+    int pos = 0;
+    bool errorFound = false;
+
+    while (inputFile.get(ch))
+    {
+        pos++;
+
+        if (ch == '{') {
+            stack.push(pos);
+        }
+        else if (ch == '}') {
+            if (stack.empty()) {
+                cout << "Ошибка: неожиданная закрывающаяся скобка } в позиции " << pos << endl;
+                errorFound = true;
+                break;
+            }
+            else {
+                stack.pop();
+            }
+        }
+    }
+
+    inputFile.close();
+
+    if (!errorFound && !stack.empty()) {
+        cout << "Ошибка: не хватает закрывающейся скобки } в позиции " << stack.top() << endl;
+        errorFound = true;
+    }
+
+    if (!errorFound) {
+        cout << "Скобки расставлены правильно" << endl;
+    }
+
+    // Записываем результат в файл и на экран
+    ofstream outputFile("out.txt");
+
+    if (!outputFile.is_open()) {
+        cout << "Не удалось создать файл" << endl;
+        return 1;
+    }
+
+    if (errorFound) {
+        outputFile << "Ошибка при проверке скобок в файле input.c";
+    }
+    else {
+        outputFile << "Скобки в файле input.c расставлены правильно";
+    }
+
+    cout << "Результат проверки сохранен в файле out.txt" << endl;
+
+    outputFile.close();
+
+    return 0;
+}
